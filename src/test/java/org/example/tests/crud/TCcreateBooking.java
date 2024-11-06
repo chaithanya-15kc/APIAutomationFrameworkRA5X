a3c8f25143137252b86d361352748ab6b421a4e7
Chaithanya <chaithanya15.1993@gmail.com>
2024-11-07 01:44:24 +0530

    Folder structure is completed

---
+++ src/test/java/org/example/tests/crud/TCcreateBooking.java
@@ -0,0 +1,12 @@
+package org.example.tests.crud;
+
+import org.testng.annotations.Test;
+
+public class TCcreateBooking {
+
+
+    @Test
+    public void testPositivePostReq() {
+
+    }
+}
---
+++ src/test/java/org/example/tests/e2e/TC1.java
@@ -0,0 +1,6 @@
+package org.example.tests.e2e;
+
+public class TC1 {
+    //my complete testcase
+    //createBooking is there, -->update Booking , --> Delete Booking
+}
